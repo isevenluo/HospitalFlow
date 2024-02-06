4ca56422f22aaf14678696c1f9cb41cbf3154953
Seven Luo <seven.luo@nexttrucking.com>
2024-02-06 16:31:41 +0800

    01-init HospitalFlow application

---
+++ src/main/java/com/seventech/hospitalflow/HospitalFlowApplication.java
@@ -0,0 +1,13 @@
+package com.seventech.hospitalflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seventech/hospitalflow/HospitalFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seventech.hospitalflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
